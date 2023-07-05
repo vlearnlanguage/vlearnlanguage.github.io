a103f5d7790333ccae0ae7ab3def64a9802c6a0d
vlearnlanguage <126780969+vlearnlanguage@users.noreply.github.com>
2023-07-05 23:39:20 +0530

    Add files via upload

--- res/var/word/unit2.js
+++ res/var/word/unit2.js
@@ -1,9 +1,10 @@
 engWord = [
     'Bye', 'Good night', 'How', 'Fine', 'Good morning', 'Hello', 'No',
-    'Sad', 'Sorry', 'Not', 'Yes', 'Thank you', ''
+    'Sad', 'Sorry', 'Not', 'Yes', 'Thank you', 'mother', 'father', 'sister', 'brother', 'granddaughter', 'this',
 ];
 hinWord = [
-    
+    'अलविदा', 'शुभ रात्रि', 'कैसे', 'ठीक है', 'सुप्रभात', 'हैलो', 'नहीं',
+    'दुखी', 'माफ करें', 'नहीं', 'हां', 'धन्यवाद', 'मां', 'पिता', 'बहन', 'भाई', 'पोती', 'यह',
 ];
 malWord = [
     
